b939f2e66f7dfcb72b4646beb1e0856bb44eed6f
QUINCY HUTCHISON <30450645+nativeB@users.noreply.github.com>
2018-07-06 14:29:16 +0000

    turn server changes

--- js/client.js
+++ js/client.js
@@ -242,40 +242,23 @@ function newuser(user) {
 
     //using Google public stun server
   //using  public stun/turn servers
-    const configuration = {
+     const configuration = {
         "iceServers": [{"urls": "stun:stun2.1.google.com:19302"},
             {"urls": "stun:stun.l.google.com:19302"},
             {"urls": "stun:stun1.l.google.com:19302"},
             {"urls": "stun:stun3.l.google.com:19302"},
             {"urls": "stun:stun4.l.google.com:19302"},
-            {"urls": "stun:stun01.sipphone.com"},
-            {"urls": "stun:stun.ekiga.net"},
-            {"urls": "stun:stun.fwdnet.net"},
-            {"urls": "stun:stun.ideasip.com"},
-            {"urls": "stun:stun.iptel.org"},
-            {"urls": "stun:stun.rixtelecom.se"},
-            {"urls": "stun:stun.schlund.de"},
-            {"urls": "stun:stunserver.org"},
-            {"urls": "stun:stun.softjoys.com"},
-            {"urls": "stun:stun.voiparound.com"},
-            {"urls": "stun:stun.voipbuster.com"},
-            {"urls": "stun:stun.voxgratia.org"},
-            {"urls": "stun:stun.xten.com]"},
+            {"urls":"stun:numb.viagenie.ca"},
             {
+                "urls": 'stun:numb.viagenie.ca',
+                credential: 'qhutchison8@gmail.com',
+                username: 'gateway08'
+            },{
                 "urls": 'turn:numb.viagenie.ca',
-                credential: 'muazkh',
-                username: 'webrtc@live.com'
-            },
-            {
-                "urls": 'turn:192.158.29.39:3478?transport=udp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808'
-            },
-            {
-                "urls": 'turn:192.158.29.39:3478?transport=tcp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808'
-            }]
+                credential: 'qhutchison8@gmail.com',
+                username: 'gateway08'
+            }
+            ]
     };
 
     yourConn[user] = new RTCPeerConnection(configuration);
